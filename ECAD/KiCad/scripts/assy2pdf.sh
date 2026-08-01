d03d89c2c88d35f2b5feec93eeb881c05fbea52d
Christoph Tack <prog.send@gmail.com>
2026-08-01 09:20:18 +0200

    Fix jobsets for output generation with variants

--- ECAD/KiCad/scripts/assy2pdf.sh
+++ ECAD/KiCad/scripts/assy2pdf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
-# Usage : sch2pdf.sh <project_name> <output_pdf_name> <revision_prefix>
+# Usage : sch2pdf.sh <project_name> <output_pdf_name> <variant_name>
+# https://docs.kicad.org/10.0/ru/cli/cli.html#pcb_export_pdf
 
 # Create folder "output" if it does not exist
 KIPRJMOD=${KIPRJMOD:-.}
@@ -11,14 +12,18 @@ new_file_name=$2"_"$git_commit_hash
 # Top side
 kicad-cli pcb export pdf ${KIPRJMOD}/$1.kicad_pcb \
     --define-var GIT_COMMIT_HASH=$git_commit_hash \
+    --variant $3 \
     --output ${KIPRJMOD}/output/$new_file_name"_ASSY_TOP.pdf" \
     --layers F.Fab,F.Silkscreen,Edge.Cuts \
     --mode-single \
-    --include-border-title
+    --include-border-title \
+    --crossout-DNP-footprints-on-fab-layers
 # Bottom side
 kicad-cli pcb export pdf ${KIPRJMOD}/$1.kicad_pcb \
     --define-var GIT_COMMIT_HASH=$git_commit_hash \
+    --variant $3 \
     --output ${KIPRJMOD}/output/$new_file_name"_ASSY_BOT.pdf" \
     --layers B.Fab,B.Silkscreen,Edge.Cuts \
     --mode-single \
-    --include-border-title
+    --include-border-title \
+    --crossout-DNP-footprints-on-fab-layers
--- ECAD/KiCad/scripts/sch2pdf.sh
+++ ECAD/KiCad/scripts/sch2pdf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
-# Usage : sch2pdf.sh <project_name> <output_pdf_name> <revision_prefix>
+# Usage : sch2pdf.sh <project_name> <output_pdf_name> <revision_prefix> <variant_name>
+# Reference : https://docs.kicad.org/10.0/ru/cli/cli.html#schematic_export_pdf
 
 # Create folder "output" if it does not exist
 KIPRJMOD=${KIPRJMOD:-.}
@@ -9,6 +10,7 @@ new_file_name=$2"_"$git_commit_hash
 
 kicad-cli sch export pdf \
     --define-var GIT_COMMIT_HASH=$git_commit_hash \
+    --variant $3 \
     --output ${KIPRJMOD}/output/$new_file_name"_SCH.pdf" \
     --theme Altium_like  \
     ${KIPRJMOD}/$1.kicad_sch
